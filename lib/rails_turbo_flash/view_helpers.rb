5bb00d571c7082ba8f867b075d8f79c8bcc7aaf9
Ryan Nevius <ryanevius@gmail.com>
2023-12-24 11:34:22 +0100

    fix: `turbo_flash_tag`

--- lib/rails_turbo_flash/view_helpers.rb
+++ lib/rails_turbo_flash/view_helpers.rb
@@ -2,8 +2,17 @@
 
 module RailsTurboFlash
   module ViewHelpers
-    def turbo_flash
-      content_tag(:div, id: 'turbo-flash')
+    # Generates the HTML tag for displaying flash messages using Turbo Streams in Ruby on Rails.
+    #
+    # @param options [Hash] The options for customizing the HTML tag.
+    # @option options [String] :id The ID attribute for the HTML tag.
+    #
+    # @return [String] The HTML tag for displaying flash messages.
+    def turbo_flash_tag(options = nil)
+      options ||= {}
+      options[:id] = 'turbo-flash'
+
+      tag.div nil, **options
     end
   end
 end
